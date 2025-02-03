8994796a7cdbb6f6a69a5f38e486aa46f807a3f3
Sid Gampa <sgampa3@gatech.edu>
2025-02-02 22:04:38 -0800

    retreieved user books from server and displayed in client

--- client/myfirstreactapp/src/App.js
+++ client/myfirstreactapp/src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Book from './Book.js';
+import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
-  const apiLink = 'http://localhost:3000/api/v1/books'
+  const apiLink = 'http://localhost:8000/api/v1/books'
+
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetch(apiLink + "/getUserBooks?uid=1", {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+      })
+      .then(res => res.json()).then(res => {setBooks(res)})}, [])
+    
+    console.log(books)
 
   // function deleteBook(id) {
   //   const res = fetch(this.apiLink + "/deleteBook", {
@@ -37,7 +52,9 @@ function App() {
 
   return (
     <div className="Book">
-      <Book title="Test1" id="1" readStatus="1" addBook={addBook}></Book>
+      <Book title="Test1" id="3" readStatus="1" addBook={addBook}></Book>
+      {books.map((book) => (<Book title={book.bookid} id={book.bookid} readStatus={book.readstatus} addBook={addBook}></Book>
+      ))}
     </div>
   );
 }
